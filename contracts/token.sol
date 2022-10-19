0e94206d77235b380e59490b4ebda4849f238d03
Luigi Bardella Gerbi <96418661+twotimesgi@users.noreply.github.com>
2022-10-19 14:33:24 +0200

    Update token.sol

--- contracts/token.sol
+++ contracts/token.sol
@@ -3,14 +3,12 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "hardhat/console.sol";
 
-contract MyToken is ERC20 {
-    uint256 constant maxMint = 100 ether;
+contract SpinToken is ERC20 {
 
     constructor() ERC20("SPIN", "SPIN") {
     }
-
     /// @dev mints a given amount of tokens to the sender
-    function transfer(address _address, uint256 _amount) public {
-        _mint(_address, _amount);
+    function transferTokens(address _address, uint256 _amount) public {
+        _mint(_address, _amount * (10**18));
     }
 }
